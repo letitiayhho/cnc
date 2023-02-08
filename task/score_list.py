dd404dfe7cc366fa2eedcaa497f066e8fae712fa
Letitia Ho <letitiayhho@uchicago.edu>
2023-02-08 11:46:01 -0600

    Improve scoring, compare first phon first

--- task/score_list.py
+++ task/score_list.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import eng_to_ipa
 
 def add_ipa(df, col, ipa_col):
     # Convert words to IPA
@@ -13,14 +14,51 @@ def add_ipa(df, col, ipa_col):
     return(df)
 
 def score_word(word_ipa, response_ipa):
-    # Compare ipa symbols in response with word
+    # Compare first phon
+    words_ipa, response_ipa, hit_phons, n_hit_phons, missed_phons = compare_first_phon(word_ipa, response_ipa)
+
+    # Compare ipa symbols for remaining phons
     word_ipa = set(word_ipa)
     response_ipa = set(response_ipa)
 
-    hit_phons = word_ipa.intersection(response_ipa)
-    n_hit_phons = len(hit_phons)
-    missed_phons = word_ipa.difference(response_ipa)
+    # Add to scores
+    hit_phons.append(list(word_ipa.intersection(response_ipa)))
+    n_hit_phons += len(hit_phons)
+    missed_phons.append(list(word_ipa.difference(response_ipa)))
+
+    hit_phons = flatten(hit_phons)
+    missed_phons = flatten(missed_phons)
+    
+    print("Comparing rest phon")
+    print(f"hit_phons: {hit_phons}")
+    print(f"n_hit_phons: {n_hit_phons}")
+    print(f"missed_phons: {missed_phons}")
+    
     return(n_hit_phons, hit_phons, missed_phons)
+
+def compare_first_phon(word_ipa, response_ipa):
+    # Compare first phon
+    if word_ipa[0] == response_ipa[0]:
+        hit_phons = [word_ipa[0]]
+        n_hit_phons = 1
+        missed_phons = []
+    else:
+        hit_phons = []
+        n_hit_phons = 0
+        missed_phons = [word_ipa[0]]
+
+    del word_ipa[0]
+    del response_ipa[0]
+
+    print("Comparing first phon")
+    print(f"hit_phons: {hit_phons}")
+    print(f"n_hit_phons: {n_hit_phons}")
+    print(f"missed_phons: {missed_phons}")
+
+    return(word_ipa, response_ipa, hit_phons, n_hit_phons, missed_phons)
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
 
 def score_all_words(df, word_ipa, response_ipa):
     n_hit_phons_list = []
